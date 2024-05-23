843de7b6361e4b8e3ec37d5c5e822e21e219be4c
David Altamirano <jdaltamiranodev@gmail.com>
2024-05-23 10:36:25 +0200

    Se añaden funciones que sirven para deshabilitar el botón de envío de formulario hasta que no se acepten las políticas, y funcionalidad para cambio de imagen haciendo click en el botón señalado

--- js/script.js
+++ js/script.js
@@ -1,1 +1,61 @@
-alert('esto es javascript');
+const btnPolitica = document.querySelector('#politica');
+const btnEnviar = document.querySelector('#enviar');
+const imagen = document.querySelector('.imagen');
+const btnCambio = document.querySelector('#cambiar');
+
+const fechaActual = document.querySelector('#fecha');
+const diaSemana = ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"];
+const meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+
+let estadoActual = 'img/ai-pin-lunar-front.jpg';
+
+
+btnPolitica.addEventListener('change', habilitarBoton);
+
+function habilitarBoton() {
+    if (btnPolitica.checked) {
+        btnEnviar.disabled = false;
+    } else {
+        btnEnviar.disabled = true;
+    }
+}
+
+function mostrarFecha() {
+    const fecha = new Date();
+    const diaMes = fecha.getDate();
+    const mesActual = fecha.getMonth();
+    const diaActual = fecha.getDay();
+    const horaActual = fecha.getHours();
+    const minutoActual = fecha.getMinutes();
+    const anio = fecha.getFullYear();
+
+    let dia = diaSemana[diaActual];
+    let mes = meses[mesActual];
+    //alert(jornada);
+
+    // Formatear horas y minutos
+    let horas = horaActual < 10 ? `0${horaActual}` : horaActual;
+    let minutos = minutoActual < 10 ? `0${minutoActual}` : minutoActual;
+
+    fechaActual.innerHTML = `${dia} ${diaMes} de ${mes} de ${anio}, ${horas}:${minutos}`;
+}
+
+mostrarFecha();
+
+btnCambio.addEventListener('click', cambiarImagen);
+
+function cambiarImagen() {
+    const imagenUno = 'img/ai-pin-lunar-front.jpg';
+    const imagenDos = 'img/ai-pin-equinox-front.jpg';
+
+    if (estadoActual === imagenUno) {
+        estadoActual = imagenDos;
+        console.log(estadoActual);
+
+    } else {
+        estadoActual = imagenUno;
+        console.log(estadoActual);
+    }
+    console.log(estadoActual);
+    imagen.src = estadoActual;
+}
